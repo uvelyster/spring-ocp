a95efa0b20ebbbed74adf9b676ccdd792dff5304
uvelyster <48861250+uvelyster@users.noreply.github.com>
2021-04-27 00:40:16 +0900

    Update SpringConfig.java

--- src/main/java/com/example/demo/SpringConfig.java
+++ src/main/java/com/example/demo/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
